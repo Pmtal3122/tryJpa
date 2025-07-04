15905bf0594b39c796983e2577b4afddb0fa8812
Pmtal3122 <talegaonkar.piyush@gmail.com>
2025-07-04 16:10:29 +0530

    Initial Commit

---
+++ src/main/java/com/piyush/tryJpa/tryJpa/TryJpaApplication.java
@@ -0,0 +1,13 @@
+package com.piyush.tryJpa.tryJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piyush/tryJpa/tryJpa/TryJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyush.tryJpa.tryJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
