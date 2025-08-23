0c771b836dfd5867ee4ffb63ac8cfeec7ac15d35
Pmtal3122 <talegaonkar.piyush@gmail.com>
2025-08-23 11:49:58 +0530

    Aise hi

--- src/main/java/com/piyush/tryJpa/tryJpa/services/BookService.java
+++ src/main/java/com/piyush/tryJpa/tryJpa/services/BookService.java
@@ -3,6 +3,7 @@ package com.piyush.tryJpa.tryJpa.services;
 import com.piyush.tryJpa.tryJpa.dto.BookAuthorDTO;
 import com.piyush.tryJpa.tryJpa.entities.BookEntity;
 import com.piyush.tryJpa.tryJpa.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.List;
 @Service
 public class BookService {
 
-//    @Autowired
+    @Autowired
     BookRepository bookRepository;
 
     public BookService(BookRepository bookRepo) {
